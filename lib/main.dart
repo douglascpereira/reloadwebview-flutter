e8a9922cd9b40355404c6a7fa5daaaa25d18494e
Douglas <douglascpereira@gmail.com>
2020-07-29 20:02:02 -0300

    projeto webview em flutter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:project_webview/reload_webview.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebView Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ReloadWebview()
+    );
+  }
+}
---
+++ lib/reload_webview.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ReloadWebview extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Reload webview in flutter")),
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
+        javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+      ),
+      floatingActionButton: FutureBuilder<WebViewController>(
+        future: _controller.future,
+        builder: (BuildContext context,
+            AsyncSnapshot<WebViewController> controller) {
+          if (controller.hasData) {
+            return FloatingActionButton(
+              onPressed: () async {
+                controller.data.reload();
+              },
+              child: const Icon(Icons.refresh),
+            );
+          }
+          return Container();
+        }),
+    );
+  }
+}
